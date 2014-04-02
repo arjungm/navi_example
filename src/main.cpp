ec85db571ee0cac796d062cc4fb8b6e33a122289
Arjun Menon <agmenon@andrew.cmu.edu>
2014-04-02 18:22:59 -0400

    rewrote to only to take command line args, construct the environment, and then plan with it

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
-#include <boost/program_options.hpp>
-#include <iostream> 
+#include <iostream>
 #include <string>
+
+#include <boost/foreach.hpp>
+#include <boost/program_options.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+#include "navi_example/Environment.h"
+
+using namespace std;
 
 int main(int argc, char** argv){
 
-  //some pretty program options
+  //some program options boilerplate
   namespace po = boost::program_options; 
   po::options_description desc("Vanilla Navigation Planner Usage"); 
   desc.add_options() 
-    ("env,e",po::value<std::string>()->required(),"input environment json file"); 
-
+    ("env,e",po::value<string>()->required(),"input environment json file"); 
   po::variables_map vm;
   po::store(po::command_line_parser(argc, argv).options(desc).run(), vm);
   po::notify(vm);
+
+  //open the json
+  string json_filename =  vm["env"].as<string>();
+  ifstream input_json_file;
+  input_json_file.open( json_filename.c_str() );
+
+  if( input_json_file ){
+    //use the json file to construct the environment
+    Environment env( input_json_file ); 
+
+    //plan on the environment
+    //Planner plnr(env);
+  }
 
   return 0;
 }
