656b53ca173e82c325a62b3307fe33f104e7eb62
Arjun Menon <agmenon@andrew.cmu.edu>
2014-04-05 15:04:53 -0400

    added construtor

--- include/navi_example/Graph.h
+++ include/navi_example/Graph.h
@@ -12,13 +12,14 @@ using namespace std;
 
 struct GraphState{
   Cell coords;
-}
+};
 
 class Graph{
   public:
     typedef boost::shared_ptr<Graph> Ptr;
     typedef boost::shared_ptr<const Graph> ConstPtr;
-
+     
+    Graph(Environment::Ptr env);
     int getHeuristicCost( const GraphState& state );
     void getValidSuccessors( const GraphState& state, vector<GraphState>& successors, vector<int>& costs );
     bool isGoalState( const GraphState& state );
