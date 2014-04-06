639db8d07c89bca37445c08143d5edd3976e717c
Arjun Menon <agmenon@andrew.cmu.edu>
2014-04-06 14:58:15 -0400

    chaned cost type return to be double

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -21,21 +21,22 @@ Graph::Graph(Environment::Ptr env) : env_(env) {
 
 }
 
-int Graph::getHeuristicCost( const GraphState::Ptr& state ){
-   int eu_dist = pow(env_->getGoal()->x - state->coords.x,2) + pow(env_->getGoal()->y - state->coords.y,2);
-   return sqrt(double(eu_dist))*100;
+double Graph::getHeuristicCost( const GraphState::Ptr& state ){
+   double eu_dist = pow(env_->getGoal()->x - state->coords.x,2) + pow(env_->getGoal()->y - state->coords.y,2);
+   return sqrt(eu_dist);
 }
 
-void Graph::getValidSuccessors( const GraphState::Ptr& state, vector<GraphState::Ptr>& successors, vector<int>& costs ){
+void Graph::getValidSuccessors( const GraphState::Ptr& state, vector<GraphState::Ptr>& successors, vector<double>& costs ){
     for(int dx=-1; dx<2; dx++){
         for(int dy=-1; dy<2; dy++){
             if(dx==0 && dx==dy){
                 continue;
             }
+            //8 connected grid
             GraphState::Ptr neighbor = boost::make_shared<GraphState>( Cell(state->coords.x+dx, state->coords.y+dy) );
             if(env_->isCollisionFree( neighbor->coords )){
                 successors.push_back(neighbor);
-                costs.push_back(100);
+                costs.push_back( sqrt(pow(dx,2)+pow(dy,2)) );
             }
         }
     }
