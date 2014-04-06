565c51483c9222baaf5bab5c0cbec61ad057fde9
Arjun Menon <agmenon@andrew.cmu.edu>
2014-04-06 15:50:28 -0400

    added file io for solutions

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <boost/foreach.hpp>
 #include <boost/make_shared.hpp>
 #include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
 
 #include "navi_example/Environment.h"
 #include "navi_example/Graph.h"
@@ -23,9 +24,14 @@ int main(int argc, char** argv){
   po::notify(vm);
 
   //open the json
-  string json_filename =  vm["env"].as<string>();
+  boost::filesystem::path json_file( vm["env"].as<string>() );
   ifstream input_json_file;
-  input_json_file.open( json_filename.c_str() );
+  if(boost::filesystem::exists(json_file)){
+     input_json_file.open( json_file.string().c_str() );
+  }
+  else{
+      printf("File \"%s\" does not exist to be read!\n", json_file.string().c_str());
+  }
 
   if( input_json_file ){
     //use the json file to construct the environment
@@ -38,9 +44,29 @@ int main(int argc, char** argv){
     Graph::Ptr graph = boost::make_shared<Graph>(env);
     Planner::Ptr plnr = boost::make_shared<Planner>(env, graph);
 
-    vector<GraphState::Ptr> path = plnr->plan();
-    for(size_t i=0;i<path.size(); i++){
-        cout << *(path[i]) << endl;
+    vector<GraphState::Ptr> path;
+    
+    //call planner
+    bool plannerResult = plnr->plan(path);
+    
+    if(plannerResult){
+        //output plan to file
+        boost::filesystem::path parent_dir = json_file.parent_path();
+        boost::filesystem::path solution_filename(json_file.stem().string()+"_sol.txt");
+        boost::filesystem::path solution_filepath = parent_dir / solution_filename;
+
+        printf("Writing out solution to: %s\n", solution_filepath.string().c_str());
+        ofstream ofs;
+        ofs.open( solution_filepath.string().c_str() );
+        
+        for(size_t i=0; i<path.size(); i++){
+            ofs << *(path[i]) << endl;
+        }
+        ofs.close();
+    }
+    else{
+        //
+        cout << "No plan found!" << endl;
     }
   }
 
